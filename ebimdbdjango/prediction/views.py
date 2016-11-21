5134fd1bc36b0aefbeea0d4bc372c843d721700f
jacob <aa>
2016-11-21 22:21:09 +0100

    upd

--- ebimdbdjango/prediction/views.py
+++ ebimdbdjango/prediction/views.py
@@ -1,17 +1,16 @@
 from django.http import HttpResponse
-from django.template import loader
-
+# from django.template import loader
+from django.shortcuts import render
 
 
 def index(request):
-    template = loader.get_template('prediction/index.html')
+    # template = loader.get_template('prediction/index.html')
 
     context = {
         'movies': [
             {"id": "id1"},
-            {"id":"id2"}
-
+            {"id": "id2"}
             ]
-
     }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'prediction/index.html', context)
