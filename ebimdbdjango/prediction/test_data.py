4c2ff935b9e7c0e18b5429eb99eaf72e673f11c0
SeanMey <afshinmehr@gmail.com>
2016-11-27 13:12:13 +0100

    update

--- ebimdbdjango/prediction/test_data.py
+++ ebimdbdjango/prediction/test_data.py
@@ -2,6 +2,11 @@ def print_some_data():
     sample_data = [
         "data1",
         "data2",
-        "data3"
+        "data3",
+        "data_test_",
+        "data_test_2"
     ]
     return sample_data
+
+def simple_foo():
+    return
